f281c48ab226af336374c35c3a4815f8620e6f7c
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-03-09 16:39:59 -0500

    idk what happened but it compiles

--- app/src/motor_task.c
+++ app/src/motor_task.c
@@ -2,8 +2,6 @@
 #include "dji_motor.h"
 
 void Motor_Task() {
-    // for all motors
-        // send motor values
-    //end for
+    
 }
 
--- app/src/robot.c
+++ app/src/robot.c
@@ -1,9 +1,12 @@
 #include "robot.h"
+
 #include "robot_tasks.h"
+#include "chassis_task.h"
 
 void Robot_Init() {
     // Initialize all hardware
-    
+    Chassis_Init();
     // Initialize all tasks
     Robot_Tasks_Start();
 }
+ 
