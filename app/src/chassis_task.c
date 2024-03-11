aa277cfa642fcbbd4974bc4fda1cc6ea3d8f0c8d
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-03-10 20:40:26 -0400

    working

--- app/src/chassis_task.c
+++ app/src/chassis_task.c
@@ -2,6 +2,13 @@
 #include "chassis_task.h"
 
 #include "dji_motor.h"
+#include "robot.h"
+#include "remote.h"
+#include "imu_task.h"
+
+extern Robot_State_t g_robot_state;
+extern Remote_t g_remote;
+extern IMU_t g_imu;
 
 DJI_Motor_Handle_t *motor_lf, *motor_rf, *motor_rr, *motor_lr;
 
@@ -12,22 +19,41 @@ void Chassis_Task_Init() {
         .reversal = MOTOR_REVERSAL_NORMAL,
         .control_mode = SPEED_CONTROL,
         .vel_unit_rpm = 1,
-        .pos_abs_ctrl = 0,
+        .pos_abs_ctrl = 1,
         .speed_pid = {
-            .kp = 2000,
+            .kp = 500,
             .ki = 0,
             .kd = 0,
-            .integral_limit = 0,
-            .output_limit = 20000,
+            .integral_limit = 5000,
+            .output_limit = 16000,
+            .dead_zone = 0,
+        },
+        .position_pid = {
+            .kp = 15000,
+            .ki = 1.2,
+            .kd = 0,
+            .integral_limit = 1000,
+            .output_limit = 16000,
             .dead_zone = 0,
         },
     };
+
     motor_config.speed_controller_id = 1;
-    motor_lf = DJI_Motor_Init(&motor_config, M2006);
-    motor_config.speed_controller_id = 2;
-    motor_rf = DJI_Motor_Init(&motor_config, M2006);
+    motor_config.control_mode = POSITION_CONTROL;
+    motor_rr = DJI_Motor_Init(&motor_config, GM6020);
 }
 
 void Chassis_Ctrl_Loop() {
-    motor_lf->set_speed(motor_lf, 1);
+    if (g_robot_state.enabled) {
+        if (g_remote.controller.right_stick.x != 0 || g_remote.controller.right_stick.y != 0) {
+            motor_rr->set_position(motor_rr, atan2(g_remote.controller.right_stick.y, g_remote.controller.right_stick.x));
+        }
+        // Control the robot
+        // motor_lf->set_speed(motor_lf, g_remote.controller.right_stick.y);
+        // motor_rf->set_speed(motor_rf, g_remote.controller.left_stick.y);
+    } else {
+        motor_rr->disable(motor_rr);
+        // motor_lf->disable(motor_lf);
+        // motor_rf->disable(motor_rf);
+    }
 }
--- app/src/robot.c
+++ app/src/robot.c
@@ -5,6 +5,11 @@
 #include "remote.h"
 #include "bsp_can.h"
 
+Robot_State_t g_robot_state = {0, 0};
+extern Remote_t g_remote;
+
+void Robot_Cmd_Loop();
+
 void Robot_Init() {
     // Initialize all hardware
     Chassis_Task_Init();
@@ -16,5 +21,15 @@ void Robot_Init() {
 
 void Robot_Ctrl_Loop() {
     // Control loop for the robot
+    Robot_Cmd_Loop();
     Chassis_Ctrl_Loop();
+}
+
+void Robot_Cmd_Loop() {
+    if (g_remote.controller.right_switch == DOWN) {
+        g_robot_state.enabled = 0;
+    } else if (g_remote.controller.right_switch == MID){
+        g_robot_state.enabled = 1;
+    }
+
 }
